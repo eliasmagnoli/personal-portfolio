fc34d6ff0382ac20cba5bc3fd7b221b26885d838
elias <eliasmagnoli16@gmail.com>
2023-08-23 18:30:08 -0300

    deploying my portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://eliasmagnoli.github.io',
+  base: '/my-portfolio-eliasmagnoli',
+});
